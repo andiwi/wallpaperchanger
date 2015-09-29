bd96de7feef7f081175db0f9330f8abcca6df8e2
unknown <andreas.wittmann.aw@gmail.com>
2015-09-29 16:02:08 +0200

    implemented wallpapers/:date

---
+++ routes/wallpapers.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var moment = require('moment');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:date', function(req, res, next) {
+  if ((moment(req.params.date, 'YYYY-MM-DD', true).isValid())) {
+
+    var options = {
+      root: __dirname + '../../imgs/'
+    }
+
+    var fileName = req.params.date + '.jpg';
+    res.sendFile(fileName, options, function(err) {
+      if (err) {
+        console.log(err);
+        res.status(err.status).send("Cannot find image");
+      } else {
+        console.log('Sent:', fileName);
+      }
+    })
+  } else {
+    res.status(400).send("Invalid date");
+  }
+});
+
+module.exports = router;

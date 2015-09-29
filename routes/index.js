1ab331922adb3c67f93be84701641777023768e7
unknown <andreas.wittmann.aw@gmail.com>
2015-09-29 17:37:05 +0200

    changed default welcome

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WallpaperChanger API' });
 });
 
 module.exports = router;

898029193cd41e717674f6b19b7d9b7fac6458b5
unknown <andreas.wittmann.aw@gmail.com>
2015-09-30 16:48:02 +0200

    changed to winston logger

--- routes/wallpapers.js
+++ routes/wallpapers.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var moment = require('moment');
+var logger = require('./utils/logger');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -13,10 +15,10 @@ router.get('/:date', function(req, res, next) {
     var fileName = req.params.date + '.JPG';
     res.sendFile(fileName, options, function(err) {
       if (err) {
-        console.log(err);
+        logger.error(err);
         res.status(err.status).send("Cannot find image");
       } else {
-        console.log('Sent:', fileName);
+        logger.info('Sent: %s', fileName);
       }
     })
   } else {
